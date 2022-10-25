71a071d0c6cdf7a808977b5b293d61ecda4841fd
keisuke0930 <keisukesudo.05@gmail.com>
2022-10-25 21:51:22 +0900

    date add

--- index.php
+++ index.php
@@ -3,3 +3,9 @@
 $result = 1+1;
 echo $result; 
 ?>
+<br>
+<?php
+echo date('Y-m-d')."<br/>\n";//現在日付 2019-12-31
+echo date('H:i:s')."<br/>\n";//現在時間 23:59:59
+echo date('Y-m-d H:i:s')."<br/>\n";//現在日時 2019-12-31 23:59:59
+?>
