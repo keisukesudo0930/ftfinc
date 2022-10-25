b9c9d943f043ccdcddcc9597c205c46c8e3229ca
keisuke0930 <keisukesudo.05@gmail.com>
2022-10-25 21:57:40 +0900

    php version add

--- index.php
+++ index.php
@@ -9,3 +9,7 @@ echo date('Y-m-d')."<br/>\n";//現在日付 2019-12-31
 echo date('H:i:s')."<br/>\n";//現在時間 23:59:59
 echo date('Y-m-d H:i:s')."<br/>\n";//現在日時 2019-12-31 23:59:59
 ?>
+<br>
+<?php
+echo 'Current PHP version: ' . phpversion();
+?>
